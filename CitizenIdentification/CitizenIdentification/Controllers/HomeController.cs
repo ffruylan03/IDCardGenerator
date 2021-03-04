5e7dd1c795d04b6842193c7d1c807289ed760ee1
ffruylan03 <ffruylan03@hotmail.com>
2021-03-04 11:56:47 +0800

    Added Source

---
+++ CitizenIdentification/CitizenIdentification/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CitizenIdentification.Models;
+using ZXing;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace CitizenIdentification.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult CitizenID()
+        {
+            var model = new CitizenIDViewModel()
+            {
+                Name = "Juan Dela D. Cruz",
+                Age = 35,
+                Address = "Door 2 Buenavista Bldg., Mandaluyong City",
+                VaccinationDate1 = DateTime.Today.ToShortDateString(),
+            };
+
+            var codeWriter = new BarcodeWriter
+            {
+                Format = BarcodeFormat.QR_CODE
+            };
+            var id = Guid.NewGuid().ToString();
+            var code = codeWriter.Write(id);
+
+            using (var memory = new MemoryStream())
+            {
+                // Store in Memory
+                code.Save(memory, ImageFormat.Png);
+
+                // Convert to Base64
+                var toBase64 = Convert.ToBase64String(memory.ToArray());
+                var toImgFile = $"data:image/png;base64,{toBase64}";
+                model.QRCode = toImgFile;
+            }
+            return View(model);
+        }
+    }
+}
---
+++ CitizenIdentification/CitizenIdentification/Models/CitizenIDViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace CitizenIdentification.Models
+{
+    public class CitizenIDViewModel
+    {
+        public string Name { get; set; }
+        public string QRCode { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+        public string VaccinationDate1 { get; set; }
+        public string VaccinationDate2 { get; set; }
+    }
+}
